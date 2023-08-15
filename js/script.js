a0d2d480a781715e271e9be0fb2f5a098330cbb3
Nahuel <ovettini@gmail.com>
2023-08-14 23:52:54 -0300

    modificando js

--- js/script.js
+++ js/script.js
@@ -30,12 +30,90 @@ function mostrar_resultado(aprobado){
     div_formulario.appendChild(resultado)
 }
 
-function chequear_idoneidad(){
-    input_submit.remove()
-    año = input_fecha.value.split("-")[0]
-    vehiculo = input_vehiculo.value
-    
-}
 
+// acá funciona el test y la fecha, pero no el vehiculo.
+// function chequear_idoneidad(año){
+//     input_submit.remove()
+//     resultado = document.createElement("h2")
+//     año = input_fecha.value.split("-")[0]
+   
+//     if(año >= 2022 && gramos_por_litro >0){
+//       resultado.innerHTML = "No podes manejar"
+//       div_formulario.classList.add("desaprobado")
+//   }
+//     if(año < 1997)
+//     {resultado.innerHTML = "Podes manejar"
+//     div_formulario.classList.add("aprobado")
+//     }
+//     if(año>1997 && año <2022 && gramos_por_litro <=0.5){
+//       resultado.innerHTML = "Podes manejar"
+//       div_formulario.classList.add("aprobado")}
+   
+//        div_formulario.appendChild(resultado)
+//     }    
+  
+
+// input_test.addEventListener("click", test_alcoholemia);
+// input_submit.addEventListener("click", chequear_idoneidad);
+
+
+function chequear_idoneidad(año){
+  input_submit.remove()
+  resultado = document.createElement("h2")
+  año = input_fecha.value.split("-")[0]
+  vehiculo = input_vehiculo.options[input_vehiculo.selectedIndex]
+
+switch(vehiculo){
+case 1:
+  if(año >= 2022 && gramos_por_litro >0){
+  resultado.innerHTML = "No podes manejar";
+  div_formulario.classList.add("desaprobado");
+}
+else if(año < 1997)
+{resultado.innerHTML = "Podes manejar"
+div_formulario.classList.add("aprobado")
+}
+else if(año>1997 && año <2022 && gramos_por_litro <=0.5){
+  resultado.innerHTML = "Podes manejar"
+  div_formulario.classList.add("aprobado");
+}
+div_formulario.appendChild(resultado)
+
+break
+
+
+ case 2:
+ if(año >= 2022 && gramos_por_litro >0){
+    resultado.innerHTML = "No podes manejar"
+    div_formulario.classList.add("desaprobado")
+  }
+  if(año < 1997)
+  {resultado.innerHTML = "Podes manejar"
+  div_formulario.classList.add("aprobado")
+  }
+  if(año>1997 && año <2022 && gramos_por_litro <=0.2){
+    resultado.innerHTML = "Podes manejar"
+    div_formulario.classList.add("aprobado")}
+    div_formulario.appendChild(resultado)
+
+  break
+ 
+     case 3:
+     if(año >= 1997 && gramos_por_litro >0){
+      resultado.innerHTML = "No podes manejar"
+      div_formulario.classList.add("desaprobado")
+    }
+    if(año < 1997)
+    {resultado.innerHTML = "Podes manejar"
+    div_formulario.classList.add("aprobado")
+    }
+    div_formulario.appendChild(resultado)
+
+       break
+  }      
+  div_formulario.appendChild(resultado)
+
+
+}
 input_test.addEventListener("click", test_alcoholemia);
 input_submit.addEventListener("click", chequear_idoneidad);
